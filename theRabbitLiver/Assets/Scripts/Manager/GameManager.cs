9039f1245a82ac60a8c01876ebf4c1b34596b105
ssunine21 <ssunine21@gmail.com>
2021-04-18 22:42:02 +0900

    0418

---
+++ theRabbitLiver/Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public LevelDesign levelDesign;
+
+	private void Awake() {
+		Singleton();
+	}
+
+	public static GameManager init;
+	private void Singleton() {
+        if (init is null) {
+            init = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else {
+            Destroy(this.gameObject);
+        }
+    }
+}
