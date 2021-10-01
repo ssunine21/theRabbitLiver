307a032093de6cc59a9bd137b553062f043746e5
ssunine21 <ssunine21@gmail.com>
2021-10-01 16:59:30 +0900

    211001

--- theRabbitLiver/Assets/Scripts/Character/Player.cs
+++ theRabbitLiver/Assets/Scripts/Character/Player.cs
@@ -2,20 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+// 스킬 : jump attack,
+// 맞거나 죽을 때 : Shoulder Hit And Fall, Head Hit
 
+public class Player : MonoBehaviour {
+	private readonly int hashMoveRight = Animator.StringToHash("moveRight");
+	private readonly int hashMoveLeft = Animator.StringToHash("moveLeft");
+
+	private readonly float CHAR_DIRECTION = 0f;
 	private readonly float TRAP_DAMAGE = 0.1f;
 	private readonly float AMOUNT_RECOVERY_HP_ON_HEALTH_ITEM = 0.3f;
 	private readonly float AMOUNT_RECOVERY_HP_ON_KILL = 0.1f;
 	private readonly float AMOUNT_RECOVERY_MP_ON_KILL = 0.2f;
 
-	public bool isStopping;
-	public bool isSuperCharge;
+
 	[Range(0, 20)]
 	public float trapCrashSpeed;
 
 	public Stamina stamina;
-	
+
+	public bool isStopping;
+	public bool isSuperCharge;
 	private bool _isCrashing;
 	public bool isCrashing {
         get { return _isCrashing; }
@@ -26,6 +33,7 @@ public class Player : MonoBehaviour {
     }
 	private float crashErrorRange = 0.05f;
 
+	private Animator animator;
 	private Vector3 crashPos;
 
 	private Character _character;
@@ -34,10 +42,13 @@ public class Player : MonoBehaviour {
         set { _character = value; }
     }
 
+    private void Start() {
+		animator = GetComponent<Animator>();
+    }
+
     private void Update() {
 		if (!isStopping) {
-			if (Input.GetKeyDown(KeyCode.LeftArrow)
-				|| Input.GetKeyDown(KeyCode.RightArrow)) {
+			if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow)) {
 				Move();
 			}
 		}
@@ -59,15 +70,23 @@ public class Player : MonoBehaviour {
 
 	private void Move() {
 		Vector3 offset = new Vector3();
+		Vector3 quaternion = new Vector3();
+
 		offset.z = this.transform.position.z + Definition.TILE_SPACING;
 		RecordData.jumpCount++;
 
 		if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+			animator.SetTrigger(hashMoveLeft);
+
+			quaternion.y = -CHAR_DIRECTION;
 			if (this.transform.position.x == 0)
 				offset.x = Definition.TILE_SPACING * 2;
 			else
 				offset.x = this.transform.position.x - Definition.TILE_SPACING;
 		} else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+			animator.SetTrigger(hashMoveRight);
+
+			quaternion.y = CHAR_DIRECTION;
 			if (this.transform.position.x == Definition.TILE_SPACING * 2)
 				offset.x = 0;
 			else
@@ -75,6 +94,7 @@ public class Player : MonoBehaviour {
 		}
 
 		this.transform.position = offset;
+		this.transform.rotation = Quaternion.Euler(quaternion);
 
 		if (RecordData.jumpCount > 50)
 			SpawnManager.init.RemoveTile();
