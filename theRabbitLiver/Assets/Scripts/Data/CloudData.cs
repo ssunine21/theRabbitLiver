fa999d552825837e290f9a8da9945995c6db9c32
ssunine21 <ssunine21@gmail.com>
2021-12-08 16:56:07 +0900

    #1208
    #shopUI and info

--- theRabbitLiver/Assets/Scripts/Data/CloudData.cs
+++ theRabbitLiver/Assets/Scripts/Data/CloudData.cs
@@ -3,18 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CloudData {
-    struct HaveCharacterInfo {
-        bool isHave;
-        int lv;
+    public class CharacterProductInfo {
+        public string Name {
+            get; set;
+        }
+        private readonly bool isPurchase;
+        private readonly int skillLevel;
+
+        public CharacterProductInfo(string name, bool isPurchase, int skillLevel) {
+            //this.name = name;
+            this.isPurchase = isPurchase;
+            this.skillLevel = skillLevel;
+        }
     }
 
-    private HaveCharacterInfo[] haveCharacterInfo;
+    public readonly ArrayList characterProductInfoList = new ArrayList();
 
     public void Load() {
+        LoadCharacterProductInfo();
 
     }
 
     public void Save() {
 
+    }
+
+    private void LoadCharacterProductInfo() {
+        CharacterProductInfo tempInfo = new CharacterProductInfo("Bunny", true, 3);
+        CharacterProductInfo tempInfo1 = new CharacterProductInfo("Skeleton", true, 1);
+        CharacterProductInfo tempInfo2 = new CharacterProductInfo("Bono", false, 0);
+
+        characterProductInfoList.Add(tempInfo);
+        characterProductInfoList.Add(tempInfo1);
+        characterProductInfoList.Add(tempInfo2);
     }
 }
