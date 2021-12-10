27503ebbae82fa2b13b9fccf82534c3e26c12d94
ssunine21 <ssunine21@gmail.com>
2021-12-10 16:51:18 +0900

    1210
    폴더정리
    shop 버튼 및 panel 변경 적용

---
+++ Assets/Scripts/UI/Shop/OnOff.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class OnOff : MonoBehaviour {
+    enum btn {
+        On, Off
+    }
+
+    public GameObject[] panelList;
+    public GameObject[] btnList;
+
+    private string[] AnimArray = { "On", "Off" };
+    private int preIndex = 0;
+    private bool isOn = false;
+    private Animation childAnim;
+
+    private void Start() {
+        childAnim = this.GetComponentInChildren<Animation>();
+    }
+
+    public void OnClick(int index) {
+        if (preIndex == index) return;
+        BtnStateChange(index);
+        PanelStateChange(index);
+        preIndex = index;
+    }
+
+    private void BtnStateChange(int index) {
+        btnList[preIndex].GetComponent<Animation>().Play(AnimArray[(int)btn.Off]);
+        btnList[index].GetComponent<Animation>().Play(AnimArray[(int)btn.On]);
+    }
+
+    private void PanelStateChange(int index) {
+        panelList[preIndex].SetActive(false);
+        panelList[index].SetActive(true);
+    }
+}
