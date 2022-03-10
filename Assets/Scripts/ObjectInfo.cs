b92299c882332e1a133f00447e611cb67df9784e
ssunine21 <ssunine21@gmail.com>
2022-03-10 16:59:59 +0900

    220308

--- Assets/Scripts/ObjectInfo.cs
+++ Assets/Scripts/ObjectInfo.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ObjectInfo : MonoBehaviour {
     public enum ItemType {
-        heart, coin, enemy
+        heart, coin, enemy, trap
     }
 
     public Vector3 offset;
