b702d57ee102dcf06815837d1b5fc66ea2da4a74
ssunine21 <ssunine21@gmail.com>
2022-05-11 13:09:39 +0900

    blender

--- Assets/Scripts/Monster/Trap/Anglerfish.cs
+++ Assets/Scripts/Monster/Trap/Anglerfish.cs
@@ -3,15 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Anglerfish : Monster {
-    private readonly float INIT_ROTATE = 135f;
-
     public GameObject lightSphere;
 
     private void Start() {
         base.SetForceKnockBack();
         animator = this.GetComponent<Animator>();
-
-        this.transform.localRotation = Quaternion.Euler(0, Time.deltaTime % 2 == 0 ? INIT_ROTATE : -INIT_ROTATE, 0);
     }
 
     private void OnTriggerStay(Collider other) {
